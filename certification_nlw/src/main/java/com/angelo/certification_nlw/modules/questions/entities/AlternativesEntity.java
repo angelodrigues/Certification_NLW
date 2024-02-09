7d03e3e73552d31635941a9be6418023a1a1ab3c
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-02-09 00:30:24 -0300

    Seed created

--- certification_nlw/src/main/java/com/angelo/certification_nlw/modules/questions/entities/AlternativesEntity.java
+++ certification_nlw/src/main/java/com/angelo/certification_nlw/modules/questions/entities/AlternativesEntity.java
@@ -1,8 +1,32 @@
 package com.angelo.certification_nlw.modules.questions.entities;
 
-import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "alternatives")
 public class AlternativesEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
--- certification_nlw/src/main/java/com/angelo/certification_nlw/modules/questions/entities/QuestionEntity.java
+++ certification_nlw/src/main/java/com/angelo/certification_nlw/modules/questions/entities/QuestionEntity.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,6 +32,8 @@ public class QuestionEntity {
 
     private String description;
 
+    @OneToMany
+    @JoinColumn(name = "question_id")
     private List<AlternativesEntity> alternatives;
 
     @CreationTimestamp
